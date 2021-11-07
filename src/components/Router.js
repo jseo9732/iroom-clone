da483831f4fded8d7f067e8a465312a7b80b4ea0
은배 <76601773+eunbae11@users.noreply.github.com>
2021-11-08 00:20:33 +0900

    Revert "add code"

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,27 +1,13 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Home, Intro, Reservation, Login, Profile } from "../pages/PagesIndex";
-import Navigation from "./Navigation";
+import { Home } from "../pages/PagesIndex";
 
 export default function AppRouter() {
   return (
     <>
       <BrowserRouter>
-        <Navigation />
         <Routes>
           <Route exact path="/" element={<Home />} />
-        </Routes>
-        <Routes>
-          <Route exact path="/intro" element={<Intro />} />
-        </Routes>
-        <Routes>
-          <Route exact path="/reservation" element={<Reservation />} />
-        </Routes>
-        <Routes>
-          <Route exact path="/login" element={<Login />} />
-        </Routes>
-        <Routes>
-          <Route exact path="/profile" element={<Profile />} />
         </Routes>
       </BrowserRouter>
     </>
--- src/pages/PagesIndex.js
+++ src/pages/PagesIndex.js
@@ -1,7 +1,3 @@
 export { default as Home } from "./Home";
 export { default as Login } from "./Login";
 export { default as Signup } from "./Signup";
-//
-export { default as Reservation } from "./Reservation";
-export { default as Intro } from "./Introduction";
-export { default as Profile } from "./Profile";
