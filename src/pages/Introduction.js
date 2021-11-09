a367dfc3be7d6aaa361b4995b8aa2025c1ed4163
username <euny_sky11@naver.com>
2021-11-09 12:05:42 +0900

    📝소개페이지 테스트 코드 추가

--- src/pages/Introduction.js
+++ src/pages/Introduction.js
@@ -1,5 +1,16 @@
 import React from "react";
 
+const onClick = () => {
+  console.log("Clicked");
+}
+
 export default function Introduction() {
-  return <div>소개 페이지</div>;
+    return ( 
+    <div>
+    <p>스터디룸 소개</p>
+    <p>취업 준비, 강의 등 모임 맞춤형 스터디룸</p>
+    <p>1인당 1,000원에 이용 가능, 2인 ~ 15인의 스터디 공간</p>
+    <img onClick={onClick} className="room4" alt="room4" src="../components/4인.jpg" />
+  </div>
+    );
 }
