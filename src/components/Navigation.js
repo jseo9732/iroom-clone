23cac3dfd01a51a3b0b609ca53eae060741ce854
jseo9732 <jseo9732@gmail.com>
2021-11-13 01:18:27 +0900

    Feat: 개선 네비게이션 클릭 시 색 변화

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,61 +1,31 @@
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
 import "./Navigation.css";
 
 export default function Navigation() {
-  const [currentClick, setCurrentClick] = useState(null);
-  const [prevClick, setPrevClick] = useState(null);
-
-  const onMenuClick = (e) => {
-    setCurrentClick(e.target.id);
-  };
-
-  useEffect(() => {
-    if (currentClick !== null) {
-      let current = document.getElementById(currentClick);
-      current.style.color = "#3eccb5";
-    }
-
-    if (prevClick !== null) {
-      let prev = document.getElementById(prevClick);
-      prev.style.color = "#ffffff";
-    }
-    setPrevClick(currentClick);
-  }, [currentClick]);
-
   return (
     <div className="HeaderContainer">
       <div className="HeaderBox">
         <Link to="/" className="HeaderTitle">
           모임공간 이룸
         </Link>
-        <ul className="HeaderMenu">
-          <li>
-            <Link to="/" id="1" onClick={onMenuClick}>
-              홈
-            </Link>
-          </li>
-          <li>
-            <Link to="/intro" id="2" onClick={onMenuClick}>
-              공간소개
-            </Link>
-          </li>
-          <li>
-            <Link to="/reservation" id="3" onClick={onMenuClick}>
-              예약하기
-            </Link>
-          </li>
-          <li>
-            <Link to="/login" id="4" onClick={onMenuClick}>
-              로그인
-            </Link>
-          </li>
-          <li>
-            <Link to="/profile" id="5" onClick={onMenuClick}>
-              마이페이지
-            </Link>
-          </li>
-        </ul>
+        <div className="HeaderMenu">
+          <NavLink to="/" className="NavBtn">
+            홈
+          </NavLink>
+          <NavLink to="/intro" className="NavBtn">
+            공간소개
+          </NavLink>
+          <NavLink to="/reservation" className="NavBtn">
+            예약하기
+          </NavLink>
+          <NavLink to="/login" className="NavBtn">
+            로그인
+          </NavLink>
+          <NavLink to="/profile" className="NavBtn">
+            마이페이지
+          </NavLink>
+        </div>
       </div>
     </div>
   );
