8cb20fe497e66b0c927f20556c592862f8f0c828
jseo9732 <jseo9732@gmail.com>
2021-11-22 21:26:34 +0900

    Feat: 추가 footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,31 @@
+import "./Footer.css";
+
 export default function Footer() {
-  return <div>footer</div>;
+  return (
+    <div className="FooterContainer">
+      <div className="FooterBox">
+        <div className="FooterLetter">
+          © 2021. Proudly created with 은배, 조은, 지수
+        </div>
+        <div className="FooterIcon">
+          <img
+            alt="Grey Twitter Icon"
+            src="https://static.wixstatic.com/media/7177d158c36d432b93f51e54f80e2f3c.png/v1/fill/w_33,h_33,al_c,q_85,usm_0.66_1.00_0.01/7177d158c36d432b93f51e54f80e2f3c.webp"
+          />
+          <img
+            alt="Grey Facebook Icon"
+            src="https://static.wixstatic.com/media/d3470ec8ca26475da4b228f0199b5d3d.png/v1/fill/w_33,h_33,al_c,q_85,usm_0.66_1.00_0.01/d3470ec8ca26475da4b228f0199b5d3d.webp"
+          />
+          <img
+            alt="Grey Google+ Icon"
+            src="https://static.wixstatic.com/media/972628489fd343de9a0961f0a1a67cfa.png/v1/fill/w_33,h_33,al_c,q_85,usm_0.66_1.00_0.01/972628489fd343de9a0961f0a1a67cfa.webp"
+          />
+          <img
+            alt="Grey Instagram Icon"
+            src="https://static.wixstatic.com/media/d7ffe259c9e54f59837481b3dd0130eb.png/v1/fill/w_33,h_33,al_c,q_85,usm_0.66_1.00_0.01/d7ffe259c9e54f59837481b3dd0130eb.webp"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
