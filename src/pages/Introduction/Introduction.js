41be1b51076f1034ef12820a9a24f23ca06e8288
username <euny_sky11@naver.com>
2021-11-28 14:08:22 +0900

    Design : Intro/RoomInfo 모바일 화면 추가

--- src/pages/Introduction/Introduction.js
+++ src/pages/Introduction/Introduction.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./Introduction.css";
 import { Link } from "react-router-dom";
-import Footer from "../../components/Footer";
 
 export default function Introduction() {
 
